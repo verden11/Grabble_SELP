d65453a9f2d064bbfd08154bd6eecaae3897e1ba
verden11 <justinas.reigis@gmail.com>
2016-11-05 12:49:19 +0000

    show user location on a map, update if location changed

---
+++ app/src/main/java/io/github/verden11/grabble/Constants/Constants.java
@@ -0,0 +1,15 @@
+package io.github.verden11.grabble.Constants;
+
+/**
+ * Created by verden on 05/11/16.
+ */
+
+public class Constants {
+
+    public class permissions {
+        public final static int ACCESS_FINE_LOCATION = 1001;
+        public final static int ACCESS_COARSE_LOCATION = 1002;
+
+    }
+
+}
--- app/src/main/java/io/github/verden11/grabble/MapsActivity.java
+++ app/src/main/java/io/github/verden11/grabble/MapsActivity.java
@@ -1,7 +1,15 @@
 package io.github.verden11.grabble;
 
+import android.*;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,18 +18,47 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.Calendar;
+import java.util.Locale;
 
-    private GoogleMap mMap;
+import io.github.verden11.grabble.Constants.Constants;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+    private final String Activity_TAG = "MapsActivity FragmentActivity";
+    private GoogleMap mMap;  // Might be null if Google Play services APK is not available.
+
+    LocationManager locationManager;
+    String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        Log.d(Activity_TAG, "onCreate");
+        askForPermission();
+        setUpMapIfNeeded();
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(), false);
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        if (location != null) {
+            onLocationChanged(location);
+        }
+
+
+    }
+
+    private void setUpMapIfNeeded() {
+        Log.d(Activity_TAG, "setUpMapIfNeeded");
+
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }
     }
 
 
@@ -36,11 +73,81 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        Log.d(Activity_TAG, "onMapReady");
+
+        // Get day of the week
+        Calendar sCalendar = Calendar.getInstance();
+        String dayLongName = sCalendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault());
+        Log.d(Activity_TAG, dayLongName);
+
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d(Activity_TAG, "onLocationChanged");
+
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+        if (mMap != null) {
+            mMap.clear();
+
+            mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title("Your location"));
+
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), 18));
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    public void askForPermission() {
+        Log.d(Activity_TAG, "askForPermission");
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                Constants.permissions.ACCESS_FINE_LOCATION);
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                Constants.permissions.ACCESS_COARSE_LOCATION);
+
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        Log.d(Activity_TAG, "onResume");
+        super.onResume();
+//        askForPermission();
+        setUpMapIfNeeded();
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(Activity_TAG, "onPause");
+        super.onPause();
+//        askForPermission();
+        locationManager.removeUpdates(this);
     }
 }
