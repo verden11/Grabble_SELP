345df57550e2622a71ea8426620383c161bc704c
verden11 <justinas.reigis@gmail.com>
2016-12-18 13:47:50 +0000

    edit SQL queries to close DB after it is no longer needed
    avoid SQL leaks

--- app/src/main/java/io/github/verden11/grabble/Helper/Queries.java
+++ app/src/main/java/io/github/verden11/grabble/Helper/Queries.java
@@ -11,48 +11,65 @@ public class Queries {
 
     public static int getIdByEmail(Activity activity, String email) {
         DbHelper mDbHelper = new DbHelper(activity);
-        // Gets the data repository in write mode
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
-        Cursor c = db.rawQuery("SELECT " + DbHelper.UsersEntry._ID + " FROM " + DbHelper.UsersEntry.TABLE_NAME + " WHERE " + DbHelper.UsersEntry.COLUMN_EMAIL + " = '" + email + "';", null);
-        c.moveToFirst();
-        int user_id = c.getInt(0);
-        c.close();
+        Cursor c = null;
+        int user_id;
+        try {
+            c = db.rawQuery("SELECT " + DbHelper.UsersEntry._ID + " FROM " + DbHelper.UsersEntry.TABLE_NAME + " WHERE " + DbHelper.UsersEntry.COLUMN_EMAIL + " = '" + email + "';", null);
+            c.moveToFirst();
+            user_id = c.getInt(0);
+        } finally {
+            c.close();
+            db.close();
+        }
         return user_id;
     }
 
     public static void addChar(Activity activity, int user_id, char ch) {
         DbHelper mDbHelper = new DbHelper(activity);
-        // Gets the data repository in write mode
         SQLiteDatabase db = mDbHelper.getWritableDatabase();
-        ch = Character.toUpperCase(ch);
-        int charCount = getCharCount(activity, user_id, ch);
-        charCount++;
+        try {
+            ch = Character.toUpperCase(ch);
+            int charCount = getCharCount(activity, user_id, ch);
+            charCount++;
 
-        ContentValues cv = new ContentValues();
-        cv.put(ch + "", charCount);
-        db.update(DbHelper.Stats.TABLE_NAME, cv, "user_id= " + user_id, null);
+            ContentValues cv = new ContentValues();
+            cv.put(ch + "", charCount);
+            db.update(DbHelper.Stats.TABLE_NAME, cv, "user_id= " + user_id, null);
+        } finally {
+            db.close();
+        }
+
     }
 
     public static int getCharCount(Activity activity, int user_id, char ch) {
         DbHelper mDbHelper = new DbHelper(activity);
-        // Gets the data repository in write mode
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
-        ch = Character.toUpperCase(ch);
-        Cursor c = db.rawQuery("SELECT " + ch + " FROM " + DbHelper.Stats.TABLE_NAME + " WHERE " + DbHelper.Stats.COLUMN_USER_ID + " = " + user_id + ";", null);
-        c.moveToFirst();
-        int count = c.getInt(0);
-        c.close();
+        Cursor c = null;
+        int count;
+        try {
+            ch = Character.toUpperCase(ch);
+            c = db.rawQuery("SELECT " + ch + " FROM " + DbHelper.Stats.TABLE_NAME + " WHERE " + DbHelper.Stats.COLUMN_USER_ID + " = " + user_id + ";", null);
+            c.moveToFirst();
+            count = c.getInt(0);
+        } finally {
+            c.close();
+        }
         return count;
     }
 
     public static long getLastKMLDownloadTime(Activity activity, int user_id) {
         DbHelper mDbHelper = new DbHelper(activity);
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
-        Cursor c = db.rawQuery("SELECT " + DbHelper.UsersEntry.COLUMN_LAST_KML_DOWNLOAD_DATE + " FROM " + DbHelper.UsersEntry.TABLE_NAME + " WHERE " + DbHelper.UsersEntry._ID + " = " + user_id + ";", null);
-        c.moveToFirst();
-        // entry does not exist
-        long epochTime = c.getLong(0);
-        c.close();
+        Cursor c = null;
+        long epochTime;
+        try {
+            c = db.rawQuery("SELECT " + DbHelper.UsersEntry.COLUMN_LAST_KML_DOWNLOAD_DATE + " FROM " + DbHelper.UsersEntry.TABLE_NAME + " WHERE " + DbHelper.UsersEntry._ID + " = " + user_id + ";", null);
+            c.moveToFirst();
+            epochTime = c.getLong(0);
+        } finally {
+            c.close();
+        }
         return epochTime;
     }
 
