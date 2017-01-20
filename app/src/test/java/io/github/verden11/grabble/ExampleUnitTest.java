510601b0062ebdb64f57dab716eeb9d90963c10a
verden11 <justinas.reigis@gmail.com>
2017-01-20 20:05:28 +0000

    tests to check word score is calculated correctly

--- app/src/test/java/io/github/verden11/grabble/ExampleUnitTest.java
+++ app/src/test/java/io/github/verden11/grabble/ExampleUnitTest.java
@@ -2,7 +2,10 @@ package io.github.verden11.grabble;
 
 import org.junit.Test;
 
+import io.github.verden11.grabble.Helper.General;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -14,4 +17,17 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void wordScore_calculate() throws Exception {
+        assertEquals(General.calculateWordValue("looking"), 70);
+        assertEquals(General.calculateWordValue("bestare"), 42);
+        assertEquals(General.calculateWordValue("Zyzomys"), 109);
+        assertEquals(General.calculateWordValue("Anomura"), 50);
+        assertEquals(General.calculateWordValue("perhaps"), 66);
+        assertEquals(General.calculateWordValue(""), 0);
+        assertNotEquals(General.calculateWordValue("abature"), 0);
+    }
+
 }
+
